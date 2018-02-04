afc89b9debdaffb75aeb449bb37d9d4eca424dc1
Elliot Dines <elliotdines1@gmail.com>
2018-02-04 10:50:19 +0000

    java code

---
+++ online/src/main/java/Test.java
@@ -0,0 +1,11 @@
+import org.opencv.core.Core;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to OpenCV " + Core.VERSION);
+
+    }
+}
